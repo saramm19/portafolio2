c6676aa8d2e8e3693951172386ac0721eb4d0379
saramm19 <sm785510@gmail.com>
2024-07-11 13:04:40 -0500

    portafolio

---
+++ script.js
@@ -0,0 +1,10 @@
+const proyectoButton = document.querySelector('#proyecto-button');
+const repositorioButton = document.querySelector('#repositorio-button');
+
+proyectoButton.addEventListener('click', () => {
+  window.open('https://github.com/saramm19/coca-cola-landing-page', '_blank');
+});
+
+repositorioButton.addEventListener('click', () => {
+  window.open('https://github.com/saramm19/coca-cola-landing-page', '_blank');
+});
